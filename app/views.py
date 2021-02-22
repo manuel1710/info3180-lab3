59746b407662980a903b673ce86e13916572f91a
Javian Anderson <javian99anderson@gmail.com>
2021-02-21 22:30:05 -0500

    Lab 3 Files

--- app/views.py
+++ app/views.py
@@ -7,6 +7,9 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+from .forms import ContactForm
+from app import mail
+from flask_mail import Message
 
 
 ###
@@ -23,6 +26,32 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+
+@app.route('/contact', methods=['GET','POST'])
+def contact():
+    contactform= ContactForm()
+    if contactform.validate_on_submit():
+        #return "Great Success"
+        name = contactform.name.data
+        email = contactform.email.data
+        subject = contactform.subject.data
+        message = contactform.message.data
+        msg = Message("{subject}", sender=("{name}","{email}"),recipients=["98ede9e5fa-48eb45@inbox.mailtrap.io"])
+        msg.body = "{message}"
+        mail.send(msg)
+        flash("Your email was successfully Sent!")
+        return render_template('home.html')
+    else:
+         return render_template('contact.html', form=contactform)
+    '''
+    if request.method == "POST":
+        name = contactform.name.data
+        email = contactform.email.data
+        subject = contactform.subject.data
+        message = contactform.message.data
+    '''
+    #return render_template('contact.html', form=contactform)
+
 
 
 ###
